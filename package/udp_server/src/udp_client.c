4c22da7e5b21059c069ad063851caa3d452dd22c
wangjianxin <wangjianxin@canaan-creative.com>
2022-09-13 12:02:08 +0800

    udp test case

---
+++ package/udp_server/src/udp_client.c
@@ -0,0 +1,67 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include<stdlib.h>
+#include <errno.h>
+
+
+#define  MAX_BUFFER_SIZE  1024
+
+
+int main(int argc, char* argv[])
+{
+
+    if (argc < 3)
+    {
+	    printf("use help:%s ip port\n",argv[0]);
+	    return -1;
+    }	    
+    char*pIP = argv[1];
+    char*pPort = argv[2];
+    //printf("ip:%s,port:%s\n",pIP,pPort);
+
+    struct sockaddr_in addr;
+    int sock;
+
+    addr.sin_family     = AF_INET;
+    addr.sin_port       = htons(atoi(pPort));
+    addr.sin_addr.s_addr = inet_addr(pIP);
+
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+	   printf("=======socket create error errno %x :%s \n", errno, strerror(errno));
+	   return -1;
+    }	
+	//printf("sock =%x\n", sock);
+	
+
+
+	char sBuffer[MAX_BUFFER_SIZE] = {0};
+	static int ncount = 0;
+	int nRet = 0;
+	for (int j = 0; ; ++j)
+	{
+		for(int i =0;i < 3000;i++)
+		{
+			memset(sBuffer,(i%100)&0xfe,MAX_BUFFER_SIZE);
+			nRet = sendto(sock, sBuffer, MAX_BUFFER_SIZE, 0, (struct sockaddr *) &addr, sizeof(addr));
+			if (nRet < 0)
+			{
+				printf("sendto error.  nRet %x errno %x : %s\n", nRet, errno, strerror(errno));
+				close(sock);
+				return -1;
+			}
+			usleep(1);
+		}
+		printf("\nprocess end j=%d  pid %d \n",j,getpid());
+	}
+	
+
+    return 0;
+}
+
---
+++ package/udp_server/src/udp_server.c
@@ -0,0 +1,125 @@
+
+
+//socket udp 服务端
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+
+#define LOCK_F "/run/udp_temp" 
+void dump_buff(char * prompt,char *buff, int len)
+{
+    int i;
+    char *p = buff;
+    if(prompt)
+        printf("%s :\n",prompt);
+
+    for (i = 0; i < len; ++i)
+    {
+        if (i%16==0)
+        {
+            printf("\n%016lx: ",(unsigned long) p+i);
+        }
+        printf("%02x ",*(p+i));
+    }
+
+}
+
+
+int main(int argc, char *argv[])
+{
+    //创建socket对象
+    int print_lock_fd =0;
+	struct flock f_lock;
+	
+    int sockfd=socket(AF_INET,SOCK_DGRAM,0);
+	unsigned long rec_pack_count=0;
+
+    //创建网络通信对象
+    struct sockaddr_in addr;
+    addr.sin_family =AF_INET;
+    addr.sin_port =htons(atoi(argv[1]));
+    addr.sin_addr.s_addr=inet_addr("0.0.0.0");
+
+	 open(LOCK_F, O_RDWR|O_CREAT);
+
+	print_lock_fd = open(LOCK_F, O_RDWR|O_CREAT);
+	if (print_lock_fd < 0)
+	{
+		return -1;
+	}
+	f_lock.l_whence = SEEK_SET;
+	f_lock.l_start = 0;
+	f_lock.l_len = 0;               /* 0表示整个文件加锁 */
+	
+
+    //绑定socket对象与通信链接
+    int ret =bind(sockfd,(struct sockaddr*)&addr,sizeof(addr));
+    if(0>ret)
+    {
+        printf("bind addr.sin_port=%d %s %d \n",addr.sin_port, argv[1], atoi(argv[1]));
+        return -1;
+
+    }
+	printf("addr.sin_port=%d %s %d\n", addr.sin_port, argv[1], atoi(argv[1]));
+    struct sockaddr_in cli;
+    socklen_t len=sizeof(cli);// 注意这个len
+    
+
+    while(1)
+    {
+        char buf[2048] ={0};
+		int i=0;
+		
+		fd_set rfds;
+		struct timeval tv;
+		int retval;
+		FD_ZERO(&rfds);
+        FD_SET(sockfd, &rfds);
+		retval = select(sockfd+1, &rfds, NULL, NULL, NULL );
+		if(retval < 0)
+		{
+			continue;
+		}
+
+		
+		
+        retval = recvfrom(sockfd,buf,sizeof(buf),0,(struct sockaddr*)&cli,&len);//这里输入客户端的地址，已经长度的**地址**
+        rec_pack_count ++;
+		if(rec_pack_count % 3000 == 0)
+		{
+			int pid=getpid();
+			f_lock.l_type = F_WRLCK;        /*选用写琐*/
+			//    f_lock.l_type = F_RDLCK;      /*选用读琐*/ 
+			fcntl(print_lock_fd, F_SETLKW, &f_lock);
+			for(i=0;i<(pid%10);i++)printf(" ");
+			printf("pid %d recv count =%ld  retval=%d buf=%lx %x %x %x %x \n", getpid(), rec_pack_count,retval, buf, buf[0],buf[1], buf[2],buf[3]);
+			f_lock.l_type = F_UNLCK;
+			fcntl(print_lock_fd, F_SETLKW, &f_lock);
+		}
+      
+        for(i=0;i<retval; i++)
+    	{
+    		if(buf[i]!=buf[0])
+			{
+				f_lock.l_type = F_WRLCK;        /*选用写琐*/
+				fcntl(print_lock_fd, F_SETLKW, &f_lock);
+				printf("error pid=%d\n", getpid());
+				dump_buff(NULL,buf, retval);
+				system("killall a.out");
+				f_lock.l_type = F_UNLCK;
+				fcntl(print_lock_fd, F_SETLKW, &f_lock);
+				break;
+			}
+    	}
+        
+    }
+    close(sockfd);
+
+}
+
